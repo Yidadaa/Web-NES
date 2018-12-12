7f830bb9692673ac864d3bf25f700d3887f50f51
Yidadaa <yidadaa@qq.com>
2018-12-12 20:42:26 +0800

    init: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <MainPlayer />
+  </div>
+</template>
+
+<script>
+import MainPlayer from './components/MainPlayer'
+
+export default {
+  name: 'app',
+  components: {
+    MainPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/MainPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ test }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      test: 'main player'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+div {
+  color: red;
+}
+</style>
